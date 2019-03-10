824b488d3b4ee5f83dcc810302e1d39384fcacf6
alexe-mgn <alexe-mgn@mail.ru>
2019-03-10 21:51:15 +0500

    added settings

---
+++ main_settings.py
@@ -0,0 +1,27 @@
+import sys
+import traceback
+import time
+
+APP_NAME = 'Game'
+EXCEPTION_FILE = 'Game_traceback.txt'
+LOAD_MEIPASS = True
+
+
+def except_hook(cls, exception, c_traceback):
+    if not getattr(sys, 'frozen', False):
+        sys.__excepthook__(cls, exception, c_traceback)
+    with open(EXCEPTION_FILE,
+              mode='a') as error_file:
+        error_file.write('\n' + time.asctime() + '\n')
+        error_file.write(str(time.time()) + 'SSTE\n')
+        error_file.write(str(cls) + '\n')
+        error_file.write(str(exception) + '\n')
+        error_file.write(''.join(traceback.format_tb(c_traceback)) + '\n')
+
+
+# For one file .exe to work
+def get_file_path(path):
+    if getattr(sys, 'frozen', False) and LOAD_MEIPASS:
+        return '\\'.join([sys._MEIPASS, path])
+    else:
+        return path
