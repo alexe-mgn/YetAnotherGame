8964d0c98a9541a90d90e8ace0a8e1807a43fc80
alexe-mgn <alexe-mgn@mail.ru>
2019-11-11 00:25:25 +0500

    Submodule support improved

--- game_main.py
+++ game_main.py
@@ -1,26 +1,35 @@
-import Engine.main
+try:
+    import Engine.main
+except ImportError as error:
+    import traceback
+
+    traceback.print_exc()
+    print("Error occurred when trying to import Engine.")
+    print("Likely submodule was not initialized")
+    print("Run update_engine.bat")
+    input("Press enter to continue")
+else:
+
+    class Main(Engine.main.Main):
+
+        def home(self):
+            """
+            Выйти в главное меню. Ну или как захотите.
+            """
+            self.set_level(None)
+            from Game.GUI import MainMenu
+            self.set_gui(MainMenu(main=self))
+
+        def load_survival(self):
+            """
+            ABOUT TO BE DELETED
+            Загрузка тестового уровня.
+            """
+            from Game.Levels.Survival import Survival
+            self.set_level(Survival(self))
+            self.set_gui(None)
 
 
-class Main(Engine.main.Main):
-
-    def home(self):
-        """
-        Выйти в главное меню. Ну или как захотите.
-        """
-        self.set_level(None)
-        from Game.GUI import MainMenu
-        self.set_gui(MainMenu(main=self))
-
-    def load_survival(self):
-        """
-        ABOUT TO BE DELETED
-        Загрузка тестового уровня.
-        """
-        from Game.Levels.Survival import Survival
-        self.set_level(Survival(self))
-        self.set_gui(None)
-
-
-if __name__ == '__main__':
-    main = Main()
-    main.start()
+    if __name__ == '__main__':
+        main = Main()
+        main.start()
---
+++ update_engine.bat
@@ -0,0 +1,10 @@
+@echo off
+if not exist .gitmodules (
+    echo submodules was not init
+    echo initializing config
+    git submodule init
+    echo done
+)
+echo updating submodules
+git submodule update
+echo done
