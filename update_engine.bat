b47722431ebb8e86863f10e18f4b2da3ac85d9e8
alexe-mgn <alexe-mgn@mail.ru>
2019-11-11 00:30:43 +0500

    Submodule support improved

--- update_engine.bat
+++ update_engine.bat
@@ -1,11 +1,11 @@
 @echo off
-if not exist .gitmodules (
-    echo submodules was not init
-    echo initializing config
-    git submodule init
-    echo done
-)
+
+echo initializing config
+git submodule init
+echo done
+
 echo updating submodules
 git submodule update
 echo done
+
 pause
