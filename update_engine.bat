c6e67e70957b65af6ca4dccb4bdd4ea1b38bd9c4
alexe-mgn <alexe-mgn@mail.ru>
2019-11-14 00:28:28 +0500

    improved engine update

--- update_engine.bat
+++ update_engine.bat
@@ -1,19 +1,3 @@
 @echo off
-
-echo initializing config
-git submodule init
-echo --done
-
-echo fetching
-git fetch
-echo --done
-
-echo updating submodules
-git submodule update
-echo --done
-
-echo performing pull from remote
-git submodule foreach git pull
-echo --done
-
-pause
+cd Engine
+call update_engine.bat & exit
